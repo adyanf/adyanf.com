ca21b45187a028e6cfcb9f6c35aecf52d3f9e4c6
Adya Naufal Fikri <adyanaufalf@gmail.com>
2024-01-20 12:29:40 +0700

    change og image

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   author: "Adya Naufal Fikri",
   desc: "A personal blog to share my thought",
   title: "Adya Naufal Fikri",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "adyanf-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
